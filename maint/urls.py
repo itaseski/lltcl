c8125e92e355b7b933a4f90efae4eb20e4cc6f06
Ilija Taseski <itaseski@t.mk>
2022-11-18 00:16:44 +0100

    list view

--- maint/urls.py
+++ maint/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('task/', views.task, name='task-list'),
+    path('works/', views.work_list, name='work-list'),
 ]
--- maint/views.py
+++ maint/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 from .models import Work
 
 
-def task(request):
-    pass
+def work_list(request):
+    works = Work.published.all()
+    contex = {
+        'works': works
+    }
+    return render(request, 'maint/work-list.html', contex)
+
+
