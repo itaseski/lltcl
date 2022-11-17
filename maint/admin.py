96def0e18907fb6dfe49f5f5830fe37bef79b401
Ilija Taseski <itaseski@t.mk>
2022-11-17 23:58:19 +0100

    adding custom manager

--- maint/admin.py
+++ maint/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Work
 
-
-admin.site.register(Work)
+@admin.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ['code','title', 'content']
+    list_display_links = ['code',]
+    list_filter = ['status']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    # raw_id_fields = ['author']
--- maint/models.py
+++ maint/models.py
@@ -6,6 +6,11 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Work.Status.PUBLISHED)
+
+
 class Work(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', _('Draft')
@@ -13,12 +18,15 @@ class Work(models.Model):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=256)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='maint_work') #  specify the name of the reverse relationship, from User to Work. 
-    code = models.CharField(max_length=101, default='XX:XX-XXXX')
+    code = models.CharField(max_length=101, default='XX-XX:XXXX')
     content = models.TextField()
     publish = models.DateTimeField(default=timezone.now) # returns the current datetime in a timezone-aware format
     created = models.DateTimeField(auto_now_add=True) #  the date will be saved automatically when creating an object
     updated = models.DateTimeField(auto_now=True) # the date will be updated automatically when saving an object.
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     class Meta:
         ordering = ['code']
