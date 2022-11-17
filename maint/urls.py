eba5b4c00cadf29652c8f9b657230a1a38df2b72
Ilija Taseski <itaseski@t.mk>
2022-11-18 00:36:51 +0100

    detail view

--- maint/urls.py
+++ maint/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('works/', views.work_list, name='work-list'),
+    path('work/<int:work_id>/', views.work_detail, name='work-detail'),
 ]
--- maint/views.py
+++ maint/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Work
 
@@ -11,3 +11,10 @@ def work_list(request):
     return render(request, 'maint/work-list.html', contex)
 
 
+
+def work_detail(request, work_id):
+    work = get_object_or_404(Work, pk=work_id, status=Work.Status.PUBLISHED)
+    contex = {
+        'work': work
+    }
+    return render(request, 'maint/work-detail.html', contex)
